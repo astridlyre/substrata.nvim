b9cb92a1003152b8ee59627d9835aba7186899f1
Erin Burton <eb@flat9.dev>
2022-01-09 13:59:06 -0800

    changed it to gray

--- lua/substrata/init.lua
+++ lua/substrata/init.lua
@@ -48,7 +48,7 @@ local set_groups = function()
     Folded = { fg = c.gray, c.none, style = "italic" },
     FoldColumn = { fg = c.blue },
     IncSearch = { style = "reverse" },
-    LineNr = { fg = c.cyan },
+    LineNr = { fg = c.gray },
     CursorLineNr = { fg = c.gray_alt },
     MatchParen = { fg = c.light_cyan, style = "bold" },
     ModeMsg = { fg = c.cyan, style = "bold" },
